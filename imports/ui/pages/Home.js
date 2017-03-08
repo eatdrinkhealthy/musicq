7d844a99f2ababcb7c7bc9301104f93af47fb870
stevenjmarsh <sjm.atl@gmail.com>
2017-03-07 20:54:16 -0500

    adding react-player package and component

--- imports/ui/pages/Home.js
+++ imports/ui/pages/Home.js
@@ -1,10 +1,12 @@
 // @flow
 import React from "react";
+import ReactPlayer from "react-player";
 import { defaultPageStyle } from "../styles";
 
 const Home = () => (
   <div className={defaultPageStyle.className}>
-    <h1>Home</h1>
+    <h1>MusicQ</h1>
+    <ReactPlayer url="https://www.youtube.com/watch?v=uUcEGOLfUTE" playing={false} controls />
   </div>
 );
 
