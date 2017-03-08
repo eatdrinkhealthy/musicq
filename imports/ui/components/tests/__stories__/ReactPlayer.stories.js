42b903c9537ee773f006036c42c583fef798b719
stevenjmarsh <sjm.atl@gmail.com>
2017-03-07 22:19:29 -0500

    adding storybook for ReactPlayer

---
+++ imports/ui/components/tests/__stories__/ReactPlayer.stories.js
@@ -0,0 +1,26 @@
+// @flow
+
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from "react";
+import {
+  storiesOf,
+} from "@kadira/storybook";
+import { text, boolean } from "@kadira/storybook-addon-knobs";
+import ReactPlayer from "react-player";
+
+storiesOf("Components", module)
+  .add("ReactPlayer - with controls", () => (
+    <ReactPlayer
+      url={text("URL", "https://www.youtube.com/watch?v=uUcEGOLfUTE")}
+      playing={boolean("Playing", false)}
+      controls
+    />
+  ))
+  .add("ReactPlayer - without controls", () => (
+    <ReactPlayer
+      url={text("URL", "https://www.youtube.com/watch?v=uUcEGOLfUTE")}
+      playing={boolean("Playing", false)}
+      controls={false}
+    />
+  ));
