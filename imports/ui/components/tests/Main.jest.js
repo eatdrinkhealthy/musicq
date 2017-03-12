6ad8a3c3494c008bc1cc926283afd2a561418b60
stevenjmarsh <sjm.atl@gmail.com>
2017-03-12 18:04:55 -0400

    adding initial test and snapshot for Main component

---
+++ imports/ui/components/tests/Main.jest.js
@@ -0,0 +1,18 @@
+// TODO   add flow
+/* eslint-env jest */
+/* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "../Main";
+
+describe("<Main />", function () {
+  it("matches render snaphot", function () {
+    const MainWithChildElement = (<Main><div>some text</div></Main>);
+
+    const tree = renderer.create(MainWithChildElement).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
