f10209e9a0325b33e002cddcf889ccef33036395
stevenjmarsh <sjm.atl@gmail.com>
2017-03-12 17:50:40 -0400

    adding initial test and snapshot for header component

---
+++ imports/ui/components/tests/Header.jest.js
@@ -0,0 +1,15 @@
+// @flow
+/* eslint-env jest */
+/* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from "react";
+import renderer from "react-test-renderer";
+import Header from "../Header";
+
+describe("<Header />", function () {
+  it("matches render snapshot", function () {
+    const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
