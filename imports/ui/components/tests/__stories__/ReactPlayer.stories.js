a7bbe267e3854e7f661975917fde9db301ac3b3e
stevenjmarsh <sjm.atl@gmail.com>
2017-03-12 18:32:49 -0400

    adding eslint-watch, clearing some flow eslint warnings (missing return types)

--- imports/ui/components/tests/__stories__/ReactPlayer.stories.js
+++ imports/ui/components/tests/__stories__/ReactPlayer.stories.js
@@ -10,14 +10,14 @@ import { text, boolean } from "@kadira/storybook-addon-knobs";
 import ReactPlayer from "react-player";
 
 storiesOf("Components", module)
-  .add("ReactPlayer - with controls", () => (
+  .add("ReactPlayer - with controls", (): React.Element<*> => (
     <ReactPlayer
       url={text("URL", "https://www.youtube.com/watch?v=uUcEGOLfUTE")}
       playing={boolean("Playing", false)}
       controls
     />
   ))
-  .add("ReactPlayer - without controls", () => (
+  .add("ReactPlayer - without controls", (): React.Element<*> => (
     <ReactPlayer
       url={text("URL", "https://www.youtube.com/watch?v=uUcEGOLfUTE")}
       playing={boolean("Playing", false)}
