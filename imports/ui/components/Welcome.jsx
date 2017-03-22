450c9a1a3f950577377e8870f6efcf9524e130f0
stevenjmarsh <sjm.atl@gmail.com>
2017-03-22 18:30:34 -0400

    adding link button to player

--- imports/ui/components/Welcome.jsx
+++ imports/ui/components/Welcome.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Welcome = () => (
-  <div>Welcome</div>
+  <div>
+    <h5>Welcome</h5>
+    <Link to="/player">
+      <span className="button">Player</span>
+    </Link>
+    <div className="row">
+      <div className="five columns offset-by-three">
+        <img src="images/music_note.png" alt="Music Note" className="u-max-full-width" />
+      </div>
+    </div>
+  </div>
 );
 
 export default Welcome;
