f67f772b6958025de2dbe0ffa48a7de32b8bd3e5
stevenjmarsh <sjm.atl@gmail.com>
2017-03-22 19:54:50 -0400

    adjusting position of Player

--- imports/ui/components/Player.jsx
+++ imports/ui/components/Player.jsx
@@ -4,15 +4,16 @@ import ReactPlayer from "react-player";
 
 const Player = (): React$Element<*> => (
   <div>
+    <br />
     <div className="row">
-      <div className="eight columns">
+      <div className="eight columns offset-by-one">
         <div className="flex-video">
           <ReactPlayer url="https://www.youtube.com/watch?v=uUcEGOLfUTE" playing={false} controls />
         </div>
       </div>
     </div>
     <div className="row">
-      <div className="eight columns">
+      <div className="eight columns offset-by-one">
         <h4>Requests</h4>
       </div>
     </div>
