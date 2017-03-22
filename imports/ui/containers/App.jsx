af9c211d1f721cef46e86576899e9e5a2d74fe31
stevenjmarsh <sjm.atl@gmail.com>
2017-03-22 17:37:10 -0400

    updating flow packages, adding App component stories and tests (and some formatting)

--- imports/ui/containers/App.jsx
+++ imports/ui/containers/App.jsx
@@ -3,20 +3,22 @@ import React from "react";
 import Header from "../components/Header";
 
 type IAppProps = {
-  children: React$Element<*>,
+  children?: ?React$Element<*>,
 };
 
 const App = (props: IAppProps): React$Element<*> => {
   const { children, ...otherProps } = props;
-  const newChild = React.cloneElement(children, otherProps);
+  const newChild = children && React.cloneElement(children, otherProps);
 
   return (
-    <div>
+    <div className="container">
       <Header />
       {newChild}
     </div>
   );
 };
+
+App.defaultProps = { children: null };
 
 export default App;
 
