1a334354be43624ca9cee7b2086e1d178ec3740b
stevenjmarsh <sjm.atl@gmail.com>
2017-03-22 19:47:09 -0400

    adding link to welcome from player (using burger)

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -1,10 +1,22 @@
 // @flow
 import React from "react";
+import { Link } from "react-router";
 
 // <img src="/images/music_note.png" alt="music note" />
 const Header = (): React$Element<*> => (
-  <div className="header">
-    <h1>MusicQ</h1>
+  <div className="row header">
+    <div className="six columns">
+      <h1>MusicQ</h1>
+    </div>
+    <div className="six columns">
+      <Link to="/" className="u-pull-right">
+        <div className="burger">
+          <div className="burger-bar" />
+          <div className="burger-bar" />
+          <div className="burger-bar" />
+        </div>
+      </Link>
+    </div>
   </div>
 );
 
